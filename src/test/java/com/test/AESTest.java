5e9d5dd59aff2963453a113ba71dd501fc3243fa
lamfire <hayash@163.com>
2015-01-15 12:14:52 +0800

    增加maven支持

--- src/test/java/com/test/AESTest.java
+++ src/test/java/com/test/AESTest.java
@@ -2,11 +2,10 @@ package com.test;
 
 import com.lamfire.code.AES;
 import com.lamfire.utils.Asserts;
-import org.junit.Test;
 
 public class AESTest{
 
-    @Test
+
 	public void testAES() {
 		String data = "融资百亿四成投红罐 白云山增资不提绿盒";
 		String key = "052E92D80523B70A";
