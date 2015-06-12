3c3300b7f1b3fba364441c7a3174e8dce24a044e
lamfire <52089489@qq.com>
2015-06-12 11:09:07 +0800

    update

--- src/test/java/com/test/RSATester.java
+++ src/test/java/com/test/RSATester.java
@@ -4,6 +4,10 @@ import java.security.KeyPair;
 
 import com.lamfire.code.Base64;
 import com.lamfire.code.RSA;
+import com.lamfire.code.RSAAlgorithm;
+import com.lamfire.utils.Asserts;
+import com.lamfire.utils.ClassLoaderUtils;
+import com.lamfire.utils.IOUtils;
 
 public class RSATester {
 	static String source = "RSA公钥加密算法是1977年由Ron Rivest、Adi Shamirh和LenAdleman在（美国麻省理工学院）开发的。RSA取名来自开发他们三者的名字。RSA是目前最有影响力的公钥加密算法，它能够抵抗到目前为止已知的所有密码攻击，已被ISO推荐为公钥数据加密标准。RSA算法基于一个十分简单的数论事实：将两个大素数相乘十分容易，但那时想要对其乘积进行因式分解却极其困难，因此可以将乘积公开作为加密密钥。";
@@ -56,7 +60,7 @@ public class RSATester {
         RSA rsa = new RSA(keySize,Base64.decode(privateKey),Base64.decode(publicKey));
 
         byte[] encodedData = rsa.encodeByPublicKey(data);
-        System.err.println("加密后文字：\n" + new String(encodedData));
+        System.err.println("加密后文字：\n" +  Base64.encode(encodedData));
         byte[] decodedData = rsa.decodeByPrivateKey(encodedData);
         String target = new String(decodedData);
         System.out.println("解密后文字: \n" + target);
@@ -65,7 +69,7 @@ public class RSATester {
         System.out.println("原文字：\n" + source);
         data = source.getBytes();
         encodedData = rsa.encodeByPrivateKey(data);
-        System.err.println("加密后：\n" + new String(encodedData));
+        System.err.println("加密后：\n" +  Base64.encode(encodedData));
         decodedData = rsa.decodeByPublicKey(encodedData);
         target = new String(decodedData);
         System.out.println("解密后: \n" + target);
@@ -81,10 +85,36 @@ public class RSATester {
         System.out.println("\n\n验证结果:" + status);
     }
 
+    static void testFile() throws Exception {
+        System.out.println("\n\n================================公钥加密——私钥解密==============================");
+        String source = IOUtils.toString(ClassLoaderUtils.getResourceAsStream("data.txt", RSAAlgorithm.class));
+        System.out.println("加密前文字：\n" + source);
+        byte[] data = source.getBytes();
+
+        RSA rsa = new RSA(keySize,Base64.decode(privateKey),Base64.decode(publicKey));
+
+        byte[] encodedData = rsa.encodeByPublicKey(data);
+        System.err.println("加密后文字：\n" + Base64.encode(encodedData));
+        byte[] decodedData = rsa.decodeByPrivateKey(encodedData);
+        String target = new String(decodedData);
+        System.out.println("解密后文字: \n" + target);
+        Asserts.equalsAssert(source,target);
+
+        System.err.println("\n\n=======================私钥加密——公钥解密=======================");
+        System.out.println("原文字：\n" + source);
+        data = source.getBytes();
+        encodedData = rsa.encodeByPrivateKey(data);
+        System.err.println("加密后：\n" +  Base64.encode(encodedData));
+        decodedData = rsa.decodeByPublicKey(encodedData);
+        target = new String(decodedData);
+        System.out.println("解密后: \n" + target);
+        Asserts.equalsAssert(source,target);
+    }
     public static void main(String[] args) throws Exception {
         keySize = 512;
         genKey();
         //test();
-        testSign();
+        //testSign();
+        testFile();
     }
 }
