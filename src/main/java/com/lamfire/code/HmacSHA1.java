a3787d1afd7feb62edc27cc639fe102441cdeabd
lamfire <52089489@qq.com>
2016-03-24 12:41:48 +0800

    fix random bub

--- src/main/java/com/lamfire/code/HmacSHA1.java
+++ src/main/java/com/lamfire/code/HmacSHA1.java
@@ -1,60 +1,28 @@
 package com.lamfire.code;
 
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-
-import javax.crypto.KeyGenerator;
 import javax.crypto.Mac;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 public class HmacSHA1 {
-	private static final String Algorithm = "HmacSHA1";
-	private KeyGenerator kg = null;
-	private Mac mac = null;
-	
-	private static HmacSHA1 instance = null;
-	
-	public static HmacSHA1 getInstance(){
-		if(instance == null){
-			try {
-				instance = new HmacSHA1();
-			} catch (NoSuchAlgorithmException e) {
-				throw new RuntimeException(e);
-			}
-		}
-		return instance;
-	}
-	
-	private HmacSHA1() throws NoSuchAlgorithmException{
-		kg = KeyGenerator.getInstance(Algorithm);
-		mac = Mac.getInstance(Algorithm);
-	}
+	private static final String HMAC_SHA1 = "HmacSHA1";
 
-	public byte[] digest(byte[] source,byte[] key) throws  InvalidKeyException{
-		kg.init(new SecureRandom(key));
-		SecretKey sk = kg.generateKey();
-		
+
+	public static byte[] digest(byte[] source,byte[] key) throws  Exception{
+		SecretKey sk  = new SecretKeySpec(key, HMAC_SHA1);
+		Mac mac = Mac.getInstance(HMAC_SHA1);
 		mac.init(sk);
 		byte[] result = mac.doFinal(source);
 		return result;
 	}
 
-	public String digest(String source,String key) throws InvalidKeyException, NoSuchAlgorithmException{
-		return Hex.encode(digest(source.getBytes(),key.getBytes()));
+	public static String hash(byte[] source,byte[] key) throws  Exception{
+		byte[] bytes = digest(source,key);
+		return Hex.encode(bytes);
 	}
 	
-	public static byte[] hash(byte[] source,byte[] key){
-		HmacSHA1 hcacSha1 = HmacSHA1.getInstance();
-		try {
-			return hcacSha1.digest(source, key);
-		} catch (InvalidKeyException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
-	public static String hash(String source,String key){
-		byte[] bytes = hash(source.getBytes(),key.getBytes());
+	public static String hash(String source,String key)throws  Exception{
+		byte[] bytes = digest(source.getBytes(),key.getBytes());
 		return Hex.encode(bytes);
 	}
 }
