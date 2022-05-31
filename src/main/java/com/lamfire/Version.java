10d9a48f0734667ad9b393e49f11a4f092c34578
hayash <hayash@163.com>
2022-05-31 21:12:04 +0800

    fix bug

--- src/main/java/com/lamfire/Version.java
+++ src/main/java/com/lamfire/Version.java
@@ -4,15 +4,8 @@ import com.lamfire.utils.JvmInfo;
 import com.lamfire.utils.NumberUtils;
 import com.lamfire.utils.StringUtils;
 
-/**
- * Version info
- * User: lamfire
- * Date: 14-3-27
- * Time: 下午5:32
- * To change this template use File | Settings | File Templates.
- */
 public class Version {
-    public static final String VERSION = "2.3.33";
+    public static final String VERSION = "2.3.34";
 
     public static String getJavaVersion() {
         JvmInfo jvm = JvmInfo.getInstance();
