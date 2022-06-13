a0487544f64c1ee41eb697f2bbd7e5e0924c3d15
root <se12345!@#$%>
2022-06-13 15:48:34 +0800

    update version to 2.3.35

--- src/main/java/com/lamfire/Version.java
+++ src/main/java/com/lamfire/Version.java
@@ -5,7 +5,7 @@ import com.lamfire.utils.NumberUtils;
 import com.lamfire.utils.StringUtils;
 
 public class Version {
-    public static final String VERSION = "2.3.34";
+    public static final String VERSION = "2.3.35";
 
     public static String getJavaVersion() {
         JvmInfo jvm = JvmInfo.getInstance();
